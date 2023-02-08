b51372ed581d9cb56cad60678f421b50b37995c8
tomnn1608 <nxt16082004@gmail.com>
2023-02-08 15:09:42 +0700

    exam

---
+++ src/exam/BookDemo.java
@@ -0,0 +1,9 @@
+package exam;
+
+public class BookDemo {
+    public static void main(String[] args) {
+        Author a = new Author("Russel","Winderand");
+        Book b = new Book("Developing Java", a,79.75);
+        System.out.println(b.toString());
+    }
+}
